bb6d51c511189560061d78a0d1f099affdff4c8a
Albert Tseng <albert.tseng@cardinalblue.com>
2023-03-18 22:35:52 +0800

    feat: add `BarChart`

---
+++ src/components/Chart/BarChart.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+type Props = {
+  value: number;
+};
+
+type BarProps = {
+  height: number;
+};
+
+const growAnimation = keyframes`
+    0% {
+        height: 0;
+    }
+`;
+
+const Bar = styled.div<BarProps>`
+  width: 48px;
+  height: ${({ height }) => height}px;
+  background-color: #2596be;
+  border-top-left-radius: 0.2rem;
+  border-top-right-radius: 0.2rem;
+  animation: ${growAnimation} 1.5s ease-in-out;
+`;
+
+function BarChart({ value }: Props) {
+  return <Bar height={value} />;
+}
+
+export default BarChart;
